90d9c87e2a9ca2d4a6ce8d2579c966f91d24e862
= <daniil.orlov16@yandex.ru>
2023-02-12 21:15:18 +0300

    Initial Commit

---
+++ LocalPosts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalPosts
+//
+//  Created by Даниил Орлов on 12.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocalPosts/LocalPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalPostsApp.swift
+//  LocalPosts
+//
+//  Created by Даниил Орлов on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct LocalPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
