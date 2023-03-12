da698898b375e94896de6d1a548619e7485f82d6
= <daniil.orlov16@yandex.ru>
2023-03-12 18:11:18 +0300

    Remove unused code

--- LocalPosts/PresentationLayer/Screens/Posts/View/Components/PostCell.swift
+++ LocalPosts/PresentationLayer/Screens/Posts/View/Components/PostCell.swift
@@ -69,28 +69,3 @@ struct PostCell: View {
         }
     }
 }
-
-public extension View {
-    func onFirstAppear(_ action: @escaping (() -> Void)) -> some View {
-        modifier(FirstAppear(action: action))
-    }
-}
-
-private struct FirstAppear: ViewModifier {
-    
-    // MARK: - Public properties
-    let action: () -> Void
-    
-    // MARK: - Private properties
-    @State private var hasAppeared = false
-    
-    // MARK: - Body
-    func body(content: Content) -> some View {
-        // And then, track it here
-        content.onAppear {
-            guard !hasAppeared else { return }
-            hasAppeared = true
-            action()
-        }
-    }
-}
